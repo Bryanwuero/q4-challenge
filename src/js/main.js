16ebb0d8f4347fea21fd6a9de118300dc5d20c44
barroyo <barroyo@brightcove.com>
2022-07-01 23:29:11 -0500

    Load Reports using scriop direct from HTML :construction: add missing styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,37 @@
-var reportsWidget = {
-    options: {
-        containerSelector: '.reports',
-        template: (
-            '{{#.}}' +
-                '<article class="reports_item">' +
-                    '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
-                    '<footer class="reports_docs">' +
-                        '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                            '</h3>' +
-                        '{{/documents}}' +
-                    '</footer>' +
-                '</article>' +
-            '{{/.}}'
-        )
-    },
+export var reportsWidget = {
+  options: {
+    containerSelector: "#reports",
+    template: `{{#.}}
+          <article class="reports_item">
+              <a href="{{cover}}" target="_blank">
+                  <img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>
+              </a>
+              <footer class="reports_docs">
+                  {{#documents}}
+                      <h3 class="reports_title">
+                          <a href="{{url}}" target="_blank">{{title}}</a>
+                      </h3>
+                  {{/documents}}
+              </footer>
+          </article>
+        {{/.}}`,
+  },
 
-    init: function() {
-        this.renderReports(reportData || []);
-    },
+  init: function () {
+    this.renderReports(reportData || []);
+  },
 
-    renderReports: function(reports) {
-        var inst = this,
-            options = inst.options;
+  renderReports: function (reports) {
+    var inst = this,
+      options = inst.options;
 
-        $(options.containerSelector).html(Mustache.render(options.template, reports));
-    }
+    var reports = $(options.containerSelector).html(
+      Mustache.render(options.template, reports)
+    );
+    var container = document.getElementById("reports");
+    console.log("container", container);
+    container.innerHTML = reports;
+  },
 };
 
 reportsWidget.init();
